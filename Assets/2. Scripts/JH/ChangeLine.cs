59b4bbd7927f48bdae8b56d8cba2b754463a3cd9
k0-juhyun <imkojuhyun@mgmail.com>
2023-09-06 20:41:08 +0900

    0906
    1. 지시등 캔버스
    2. 차 모델 변경

---
+++ Assets/2. Scripts/JH/ChangeLine.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1차선에 있다가 2차선으로 차선을 변경할때
+// 즉 중앙선을 넘어갈때
+// 깜빡이가 안켜져 있으면 감점
+public class ChangeLane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
