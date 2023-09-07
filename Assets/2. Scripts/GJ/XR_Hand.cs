f3a0b89c456f268bbe77a8b3019c0be2f50b096b
21-117 <sssa011@naver.com>
2023-09-07 15:35:11 +0900

    핸드 컨트롤러 추가

---
+++ Assets/2. Scripts/GJ/XR_Hand.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XR_Hand : MonoBehaviour
+{
+    #region Public Variables
+    public InputDeviceCharacteristics controllerType;
+    public GameObject handModelPrefab;
+    public bool showCont = false;
+    #endregion
+
+    #region Private Variables
+    private InputDevice cont;
+    private Animator handAni;
+    private bool _isControllerFound;
+    private GameObject spawnedHandModel;
+    #endregion
+
+    void Init()
+    {
+        List<InputDevice> xrDevices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerType, xrDevices);
+
+        if (xrDevices.Count.Equals(0))
+        {
+            //Debug.Log("디바이스 감지가 안되었습니다.");
+        }
+        else
+        {
+            if (xrDevices.Count > 0)
+            {
+                cont = xrDevices[0];
+                spawnedHandModel = Instantiate(handModelPrefab, transform);
+                handAni = spawnedHandModel.GetComponent<Animator>();
+                _isControllerFound = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!_isControllerFound)
+        {
+            Init();
+        }
+        else
+        {
+            UpdateHandAni();
+        }
+    }
+
+    void UpdateHandAni()
+    {
+        if (cont.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAni.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAni.SetFloat("Trigger", 1);
+        }
+
+        if (cont.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAni.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAni.SetFloat("Grip", 1);
+        }
+    }
+}
