e6dfb57cecbe040fd9ae4b04f90e5cf716199e2f
xalswox <qoalswo123@icloud.com>
2023-09-08 16:33:15 +0900

    Feat: Car Wheel Colider 추가 및 페달 생성

---
+++ Assets/2. Scripts/MJ/WheelController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    //wheelColider 4개를 가져온다.
+    public WheelCollider[] wheelColliders = new WheelCollider[4];
+
+    //가속 힘을 지정한다.
+    public float acceleration = 500f, breakingForce = 300f, maxTurnAngle = 15f;
+
+    //현재 가속도, 현재 정지힘을 0으로 초기화한다.
+    private float currentAcceleration = 0f, currentBreakForce = 0f, currentTurnAngle;
+
+    private void FixedUpdate()
+    {
+        //자동차 가속을 설정한다.
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentBreakForce = breakingForce;
+        }
+        else
+        {
+            currentBreakForce = 0f;
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            //앞 뒤 방향 속도 지정
+            wheelColliders[i].motorTorque = currentAcceleration;
+            //좌우 회전 각 방향 지정
+            wheelColliders[i].steerAngle = currentTurnAngle;
+        }
+
+        //앞 뒤바퀴의 모든 토크를 currentbreakforece로 설정한다.
+        foreach (WheelCollider wheelCollider in wheelColliders)
+        {
+            wheelCollider.brakeTorque = currentBreakForce;
+        }
+    }
+}
