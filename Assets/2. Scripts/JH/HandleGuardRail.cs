71d8a5af460908d35a83594e5746be31505f234f
k0-juhyun <imkojuhyun@gmail.com>
2023-09-13 16:13:12 +0900

    사운드 매니저, 경사로 정지 제작
    사운드 매니저, 경사로 정지 제작

---
+++ Assets/2. Scripts/JH/HandleGuardRail.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleGuardRail : MonoBehaviour
+{
+    private int contactAccidentScore = 15;
+
+    // 차로 준수 위반
+    // 가드레일에 충돌하면 감점
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("가드레일 충돌 감점");
+        // 감점
+        ScoreManager.instance.Deduction(contactAccidentScore);
+    }
+}
---
+++ Assets/2. Scripts/JH/HandleSuddenStop.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleSuddenStop : MonoBehaviour
+{
+    private JHCarTest carTest;
+
+    private int suddenStopScore = 10;
+
+    private float breakLimitTime = 2;
+    private float lightLimitTime = 3;
+
+    // 브레이크를 누른적이 있는지 확인
+    private bool isBreakPushed;
+
+    // 돌발등이 켜졌을때
+    private void OnTriggerStay(Collider other)
+    {
+        carTest = other.GetComponentInParent<JHCarTest>();
+
+        if (carTest.isBreak)
+            isBreakPushed = true;
+        
+        // 2 초이내에 브레이크를 누르지못한 경우
+        while (!carTest.isBreak && breakLimitTime > 0 && !isBreakPushed)
+        {
+            breakLimitTime -= Time.deltaTime;
+
+            if (breakLimitTime <= 0)
+            {
+                ScoreManager.instance.Deduction(suddenStopScore);
+                print("2초 이내에 브레이크 밟지 않아서 감점: " + suddenStopScore);
+            }
+            break;
+        }
+
+        // 정지 후 3초 이내에 비상깜빡이 키지 않은 경우
+        if (carTest.isBreak)
+        {
+            while (!carTest.isHazardWarningLight && lightLimitTime > 0)
+            {
+                lightLimitTime -= Time.deltaTime;
+
+                if (lightLimitTime <= 0)
+                {
+                    ScoreManager.instance.Deduction(suddenStopScore);
+                    print("3초 이내에 비상깜빡이 키지않아서 감점: " + suddenStopScore);
+                }
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 다시 출발 시 비상깜빡이를 끄지 않은 경우
+        if (carTest.isHazardWarningLight 
+            && other.gameObject.name == "CarTrqigger")
+        {
+            ScoreManager.instance.Deduction(suddenStopScore);
+            print("출발 시 비상깜빡이 끄지않아서 감점: " + suddenStopScore);
+        }
+    }
+}
