3955f59681395f43229d25ca11f54db4dfe99555
k0-juhyun <imkojuhyun@gmail.com>
2023-09-14 19:19:25 +0900

    신호 위반 구현 완료
    신호 위반 구현 완료

---
+++ Assets/2. Scripts/JH/HandleTrafficCollider.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 신호가 빨간 불일 때 들어오면 실격
+public class HandleTrafficCollider : MonoBehaviour
+{
+    private WheelController wheelController;
+
+    public bool signalViolation;
+
+    // 신호가 빨간 불 일때 밟으면 실격
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name);
+        wheelController = other.GetComponentInParent<WheelController>();
+
+        if(wheelController != null)
+        {
+            print(other.gameObject.name);
+            signalViolation = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        wheelController = other.GetComponentInParent<WheelController>();
+
+        if (wheelController != null)
+        {
+            print(other.gameObject.name);
+            signalViolation = false;
+        }
+    }
+}
+
+
--- Assets/2. Scripts/JH/HandleTrafficLight.cs
+++ Assets/2. Scripts/JH/HandleTrafficLight.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -5,8 +6,11 @@ using UnityEngine;
 
 public class HandleTrafficLight : MonoBehaviour
 {
-    public GameObject[] trafficLight1; // 첫 번째 신호등 오브젝트
-    public GameObject[] trafficLight2; // 두 번째 신호등 오브젝트
+    public GameObject[] evenTrafficLight; // 첫 번째 신호등 오브젝트
+    public GameObject[] oddTrafficLight; // 두 번째 신호등 오브젝트
+
+    public GameObject[] evenTrafficCollider; // 콜라이더
+    public GameObject[] oddTrafficCollider; // 콜라이더
 
     private TrafficLightState state1 = TrafficLightState.Green;
     private TrafficLightState state2 = TrafficLightState.Red;
@@ -17,6 +21,15 @@ public class HandleTrafficLight : MonoBehaviour
     private float timer1 = 0f;
     private float timer2 = 0f;
 
+    public bool[] even;
+    public bool[] odd;
+
+    private void Awake()
+    {
+        even = new bool[evenTrafficLight.Length];
+        odd = new bool[oddTrafficLight.Length];
+    }
+
     private enum TrafficLightState
     {
         Green,
@@ -26,6 +39,7 @@ public class HandleTrafficLight : MonoBehaviour
 
     private void Update()
     {
+        #region 신호등 로직
         // 첫 번째 신호등 상태 업데이트
         timer1 += Time.deltaTime;
         switch (state1)
@@ -35,8 +49,8 @@ public class HandleTrafficLight : MonoBehaviour
                 {
                     state1 = TrafficLightState.Yellow;
                     timer1 = 0f;
-                    SetTrafficLightColor(trafficLight1[0], Color.yellow);
-                    SetTrafficLightColor(trafficLight1[1], Color.yellow);
+                    SetTrafficLightColor(evenTrafficLight[0], Color.yellow);
+                    SetTrafficLightColor(evenTrafficLight[1], Color.yellow);
                 }
                 break;
             case TrafficLightState.Yellow:
@@ -44,8 +58,8 @@ public class HandleTrafficLight : MonoBehaviour
                 {
                     state1 = TrafficLightState.Red;
                     timer1 = 0f;
-                    SetTrafficLightColor(trafficLight1[0], Color.red);
-                    SetTrafficLightColor(trafficLight1[1], Color.red);
+                    SetTrafficLightColor(evenTrafficLight[0], Color.red);
+                    SetTrafficLightColor(evenTrafficLight[1], Color.red);
                 }
                 break;
             case TrafficLightState.Red:
@@ -53,8 +67,8 @@ public class HandleTrafficLight : MonoBehaviour
                 {
                     state1 = TrafficLightState.Green;
                     timer1 = 0f;
-                    SetTrafficLightColor(trafficLight1[0], Color.green);
-                    SetTrafficLightColor(trafficLight1[1], Color.green);
+                    SetTrafficLightColor(evenTrafficLight[0], Color.green);
+                    SetTrafficLightColor(evenTrafficLight[1], Color.green);
                 }
                 break;
         }
@@ -68,8 +82,8 @@ public class HandleTrafficLight : MonoBehaviour
                 {
                     state2 = TrafficLightState.Yellow;
                     timer2 = 0f;
-                    SetTrafficLightColor(trafficLight2[0], Color.yellow);
-                    SetTrafficLightColor(trafficLight2[1], Color.yellow);
+                    SetTrafficLightColor(oddTrafficLight[0], Color.yellow);
+                    SetTrafficLightColor(oddTrafficLight[1], Color.yellow);
                 }
                 break;
             case TrafficLightState.Yellow:
@@ -77,8 +91,8 @@ public class HandleTrafficLight : MonoBehaviour
                 {
                     state2 = TrafficLightState.Red;
                     timer2 = 0f;
-                    SetTrafficLightColor(trafficLight2[0], Color.red);
-                    SetTrafficLightColor(trafficLight2[1], Color.red);
+                    SetTrafficLightColor(oddTrafficLight[0], Color.red);
+                    SetTrafficLightColor(oddTrafficLight[1], Color.red);
                 }
                 break;
             case TrafficLightState.Red:
@@ -86,11 +100,55 @@ public class HandleTrafficLight : MonoBehaviour
                 {
                     state2 = TrafficLightState.Green;
                     timer2 = 0f;
-                    SetTrafficLightColor(trafficLight2[0], Color.green);
-                    SetTrafficLightColor(trafficLight2[1], Color.green);
+                    SetTrafficLightColor(oddTrafficLight[0], Color.green);
+                    SetTrafficLightColor(oddTrafficLight[1], Color.green);
                 }
                 break;
         }
+        #endregion
+
+        #region 신호 위반
+        // 같은 신호등 (짝수/홀수) 의 신호가 빨간 불일때 
+        // 트리거 충돌 했다면 실격
+        // 짝수 신호등이 빨간 불일때
+
+        for (int i = 0; i < evenTrafficLight.Length; i++)
+        {
+            even[i] = evenTrafficCollider[i].GetComponent<HandleTrafficCollider>().signalViolation;
+        }
+
+        for (int i = 0; i < oddTrafficCollider.Length; i++)
+        {
+
+            odd[i] = oddTrafficCollider[i].GetComponent<HandleTrafficCollider>().signalViolation;
+        }
+
+        if (state1 == TrafficLightState.Red)
+        {
+            for(int i = 0; i< even.Length ; i++) 
+            {
+                // 신호 위반을 했다면
+                if(even[i])
+                {
+                    print(even[i]);
+                    ScoreManager.instance.disqulification = true;
+                }
+            }
+        }
+
+        else if (state2 == TrafficLightState.Red)
+        {
+            for (int i = 0; i < odd.Length; i++)
+            {
+                // 신호 위반을 했다면
+                if (odd[i])
+                {
+                    print(odd[i]);
+                    ScoreManager.instance.disqulification = true;
+                }
+            }
+        }
+        #endregion
     }
 
     private void SetTrafficLightColor(GameObject trafficLight, Color color)
