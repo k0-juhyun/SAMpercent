71b45f0fdde442e36918beb87e35eb31d5737621
21-117 <sssa011@naver.com>
2023-09-15 13:44:16 +0900

    이벤트 매니저 생성..

--- Assets/2. Scripts/Common/SAMPRO_EventManager.cs
+++ Assets/2. Scripts/Common/SAMPRO_EventManager.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static SAMPRO_EventManager;
 
 public class SAMPRO_EventManager : MonoBehaviour
 {
     public static SAMPRO_EventManager instance;
 
     public delegate void CallbackEvent();
-
     public static event CallbackEvent eventCallBack;
 
+    public Hashtable eventHash = new Hashtable();
+    
     private void SetSingleton()
     {
         if (instance != null) Destroy(instance);
@@ -19,5 +22,16 @@ public class SAMPRO_EventManager : MonoBehaviour
     private void Awake()
     {
         SetSingleton();
+    }
+
+    public void AddCallBackEvent(Enumeration.GearEventType gearType, CallbackEvent _event)
+    {
+        //var callBacks = (List<CallBackEvent>)eventHash[gearType];
+        //if (callBacks == null)
+        //{
+        //    callBacks = new List<CallBackEvent>();
+        //    eventHash.Add(gearType, callBacks);
+        //}
+        //callBacks.Add(_event);
     }
 }
--- Assets/2. Scripts/GJ/GearBox.cs
+++ Assets/2. Scripts/GJ/GearBox.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GearBox : MonoBehaviour
 {
     public static GearBox instance;
+
+    Enumeration.GearEventType gearType;
 
 
     private void Awake()
@@ -14,8 +17,39 @@ public class GearBox : MonoBehaviour
         instance = this;
     }
 
-    void FixedUpdate()
+    private void Start()
+    {
+        AddEvent();
+    }
+    private void OnDestroy()
+    {
+        RemoveEvent();
+    }
+
+    private void AddEvent()
+    {
+        //SAMPRO_EventManager.instance
+    }
+
+    private void RemoveEvent()
     {
 
+    }
+
+    public void onDrive()
+    {
+        gearType = Enumeration.GearEventType.eDrive;
+    }
+    public void onNeutral()
+    {
+        gearType = Enumeration.GearEventType.eNeutral;
+    }
+    public void onReverse()
+    {
+        gearType = Enumeration.GearEventType.eReverse;
+    }
+    public void onParking()
+    {
+        gearType = Enumeration.GearEventType.eParking;
     }
 }
